40de9edc9522dac6255eec882b9f2f4e661a920f
feild12-r <h2@spaghettiverse.com>
2024-11-06 01:20:34 +0000

    1.25.3 Test

--- js/player.js
+++ js/player.js
@@ -42,7 +42,7 @@ export default class Player {
     this.level = 1;
     this.realm = 1;
     //qwert
-    this.spiritStones = 10000000;
+    this.spiritStones = 1000000000;
     this.spiritStonesMultiplier=1;
     this.talent = 1;
     this.BaseClickPower = 0.5;
